765f2174004472437524922965f2ac0aa610c3e9
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-11-30 17:01:03 -0800

    A function Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,27 +1,7 @@
-#include "main.h"
+#!/usr/bin/python3
+# Author - Gabriel Dan
 
-/**
-* wildcmp - Compare two strings allowing for wildcard char
-*
-* @s1: String being compared
-*
-* @s2: String being compared against
-*
-* Return: 1 if considered identical, 0 otherwise
-*/
-
-int wildcmp(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-			return (1);
-	}
-	return (0);
-}
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
